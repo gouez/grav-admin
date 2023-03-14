b21adb7fc227748cbf52bc804ad8932b352f0274
gouqingsong <gqsforme@gmail.com>
2023-03-14 15:47:00 +0800

    add dependencies

--- public/index.php
+++ public/index.php
@@ -1,3 +1,12 @@
 <?php
 
-echo 333;
+$autoload = __DIR__ . '/../vendor/autoload_runtime.php';
+if (!file_exists($autoload)){
+    die('Preparing to install vendor dependencies...');
+}
+
+require_once __DIR__ . '/../vendor/autoload_runtime.php';
+
+return function (array $context) {
+
+}
